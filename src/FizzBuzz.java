fbd98738936d59e9d4feaa3c45a14cc8ceffc5c0
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-24 17:14:39 -0400

    SecondCommit

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,9 @@ public class FizzBuzz {
         int number = 1;
         int stop = 100;
         for (; number <= 100; number++) {
-            if(number % 3 == 0) {
+            if(number % 3 == 0 && number % 5 ==0) {
+                System.out.println("FizzBuzz");
+            }else if (number % 3 ==0){
                 System.out.println("Fizz");
             }else if (number % 5 ==0){
                 System.out.println("Buzz");
